13f47439d15d6ca9ff3912a285b394afdd792bb8
Daniel Bogucki <daniel.bogucki14@gmail.com>
2022-02-17 13:27:21 +0100

    Added play fn, fixed logic tweaks

--- scripts.js
+++ scripts.js
@@ -1,29 +1,32 @@
 const rgb = document.querySelector('.rgb');
 const squares = document.querySelectorAll('.square');
 const scoreDisplay = document.querySelector('.score');
-const message = document.querySelector('.message');
+const message = document.querySelector('#message');
 const modes = document.querySelectorAll('.mode');
 const resetButton = document.querySelector('.reset');
 const headline = document.querySelector('.headline');
+const options = document.querySelector('.options');
+const oldHTML = headline.innerHTML;
 
-let squareNum = 6;
+let squareNum = 3;
 let score = 0;
 let colors = [];
 let pickedColor;
 
 function init() {
-    score = 0;
     colors = [];
+    restoreDefalut();
     
     if (squareNum == 3) {
         for (i = 0; i < squareNum; i++){
             squares[i].style.display = 'none';
         }
     } else {
-        squares.forEach(square => square.style.display = 'block');
+        squares.forEach(square => square.style.display = '');
     }
     generateRandomColors();
     pickColor();
+    const rgb = document.querySelector('.rgb');
     rgb.textContent = pickedColor.toUpperCase();
     
 }
@@ -31,18 +34,18 @@ function init() {
 function gameMode() {
     const mode = this.textContent;
     mode == 'EASY' ? squareNum = 3 : squareNum = 6;
-    init();
+    reset();
 }
 
 function pickColor() {
-    const random = Math.round(Math.random() * colors.length);
+    const random = Math.round(Math.random() * (colors.length-1));
     return pickedColor = colors[random];
 }
 
 function generateRandomColors() {
     squares.forEach(square => {
         square.style.backgroundColor = makeColor();
-        if(square.style.display == 'block'){colors.push(square.style.backgroundColor);}
+        if(!square.style.display){colors.push(square.style.backgroundColor);}
     })
 }
 
@@ -54,8 +57,51 @@ function makeColor() {
     return `rgb(${r},${g},${b})`;
 }
 
+function restoreDefalut() {
+    headline.innerHTML = oldHTML;
+    headline.style.borderColor = '';
+    headline.style.color = '';
+    options.style.backgroundColor = '';
+}
+
+function play() {
+    let playerColor = this.style.backgroundColor;
+
+    if (playerColor == pickedColor) {
+        squares.forEach(square => square.style.backgroundColor = '#008000');
+        headline.innerHTML = `<br>CORRECT!<br><br>`;
+        headline.style.borderColor = '#008000';
+        headline.style.color = '#008000';
+        options.style.backgroundColor = '#008000';
+
+        score++;
+        scoreDisplay.textContent = `Score: ${score}`;
+
+        setTimeout(init, 1000);
+    } else {
+        squares.forEach(square => square.style.backgroundColor = '#880808');
+        headline.innerHTML = `<br>WRONG!<br><br>`;
+        headline.style.borderColor = '#880808';
+        headline.style.color = '#880808';
+        options.style.backgroundColor = '#880808';
+
+        score = 0;
+        scoreDisplay.textContent = `Score: ${score}`;
+
+        setTimeout(init, 1000);
+    }
+}
+
+function reset() {
+    score = 0;
+    scoreDisplay.textContent = `Score: ${score}`;
+
+    init();
+}
+
 modes.forEach(mode => mode.addEventListener('click', gameMode));
-resetButton.addEventListener('click', init);
+resetButton.addEventListener('click', reset);
+squares.forEach(square => square.addEventListener('click', play));
 
 init();
 
